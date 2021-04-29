7ed78721376fdcae66f4b729096e6a5d13f3d1ff
APlatonenkov <platonenkov87@gmail.com>
2021-04-29 17:54:11 +0300

    setting to config

--- Sample/BlazorTestProject/Program.cs
+++ Sample/BlazorTestProject/Program.cs
@@ -23,16 +23,16 @@ namespace BlazorTestProject
             {
                 // Configure your authentication provider options here.
                 // For more information, see https://aka.ms/blazor-standalone-auth
-                //builder.Configuration.Bind("Local", options.ProviderOptions);
-                options.ProviderOptions.Authority = "https://localhost:44310";
-                options.ProviderOptions.ClientId = "BlazorTestAppId";
-                options.ProviderOptions.DefaultScopes.Add("profile");
-                options.ProviderOptions.DefaultScopes.Add("email");
-                options.ProviderOptions.DefaultScopes.Add("openid");
-                options.ProviderOptions.ResponseType = "code";
+                builder.Configuration.Bind("Local", options.ProviderOptions);
+                //options.ProviderOptions.Authority = "https://localhost:44310";
+                //options.ProviderOptions.ClientId = "BlazorTestAppId";
+                //options.ProviderOptions.DefaultScopes.Add("profile");
+                //options.ProviderOptions.DefaultScopes.Add("email");
+                //options.ProviderOptions.DefaultScopes.Add("openid");
+                //options.ProviderOptions.ResponseType = "code";
 
-                options.ProviderOptions.RedirectUri = "https://localhost:44372/authentication/login-callback";
-                options.ProviderOptions.PostLogoutRedirectUri = "https://localhost:44372/authentication/logout-callback";
+                //options.ProviderOptions.RedirectUri = "https://localhost:44372/authentication/login-callback";
+                //options.ProviderOptions.PostLogoutRedirectUri = "https://localhost:44372/authentication/logout-callback";
             });
 
             await builder.Build().RunAsync();
