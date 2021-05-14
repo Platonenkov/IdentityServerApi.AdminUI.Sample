731f414fee41bfba69f92e616be22e6fa368f293
APlatonenkov <platonenkov87@gmail.com>
2021-05-14 08:48:50 +0300

    Blazor test pages

--- Sample/BlazorTestProject/Program.cs
+++ Sample/BlazorTestProject/Program.cs
@@ -18,7 +18,6 @@ namespace BlazorTestProject
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            
             builder.Services.AddOidcAuthentication(options =>
             {
                 // Configure your authentication provider options here.
